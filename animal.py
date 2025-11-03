347a40d2611374709e313c386e69c3137dacc8a5
Roshani <dhiry012@mymail.unisa.edu.au>
2025-11-04 09:51:01 +1030

    Update docstrings, add note numbers to Animal report method, and create method to remove a note.

--- animal.py
+++ animal.py
@@ -143,6 +143,56 @@ class Animal(ABC):
         # Print confirmation message
         print("Note successfully added.\n")
 
+    def remove_note(self):
+        '''
+        This method deletes a specified note from the animal's individual record.
+        The animal's report is displayed and the user is prompted to enter which note they want to remove. It must be in
+        the format "category-index". For example, "injuries-2" or "behavioural concerns-4".
+        The user can enter "e" to exit the method and no changes will be made to the animal's record.
+        If a valid note is selected, the note is permanently removed from the animal's record.
+        This method takes no parameters and returns nothing.
+        '''
+        # Show report for animal.
+        self.report()
+
+        # Get animal's record.
+        animal_record = Animal.__notes.get(f"{self.__species}-{self.__id}")
+
+        # Get note to delete (category-index).
+        delete_note = input("Enter note to delete (eg injuries-1, e to exit): ")
+        delete_note_split = delete_note.split("-")
+        if len(delete_note_split) == 2 and delete_note_split[0] == "behavioural concerns":
+            delete_note_split[0] = "behavioural_concerns"
+
+        # Validate input.
+        while (delete_note != "e" and (len(delete_note_split) != 2 or delete_note_split[0] not in ["injuries", "illnesses", "behavioural_concerns"]
+                or not delete_note_split[1].isdigit() or int(delete_note_split[1]) == 0 or
+                int(delete_note_split[1]) > len(animal_record.get(delete_note_split[0])))):
+
+            if (len(delete_note_split) == 2 and delete_note_split[0] in ["injuries", "illnesses", "behavioural_concerns"]
+                and delete_note_split[1].isdigit() and len(animal_record.get(delete_note_split[0])) == 0):
+                print("There are no notes for that category. Please choose another category or enter e to exit.")
+            elif (len(delete_note_split) == 2 and delete_note_split[0] in ["injuries", "illnesses", "behavioural_concerns"]
+                and delete_note_split[1].isdigit() and int(delete_note_split[1]) > len(animal_record.get(delete_note_split[0]))):
+                print(f"That category only has {len(animal_record.get(delete_note_split[0]))} note(s). Please enter a valid index.")
+            else:
+                print("Invalid entry. Please use the example format.")
+
+            delete_note = input("Enter note to delete (eg injuries-1, e to exit): ")
+            delete_note_split = delete_note.split("-")
+            if len(delete_note_split) == 2 and delete_note_split[0] == "behavioural concerns":
+                delete_note_split[0] = "behavioural_concerns"
+
+        # If valid note selected (ie if user didn't exit), delete note.
+        if delete_note != "e":
+            key = delete_note_split[0]
+            value = animal_record.get(key)
+            del value[int(delete_note_split[1]) - 1]
+            animal_record.update({key: value})
+            Animal.__notes.update({f"{self.__species}-{self.__id}": animal_record})
+            print("Note removed successfully.")
+        print()
+
     def report(self):
         '''
         This method prints the individual animal's current record.
@@ -156,28 +206,38 @@ class Animal(ABC):
         if animal_record.get("injuries") == []:
             injuries += "None\n\n"
         else:
+            count = 1
             for note in animal_record.get("injuries"):
-                injuries += f"Description: {note[0]}\nDate reported: {note[1]}\nSeverity: {note[2]}\nNotes: {note[3]}\n\n"
+                injuries += f"{count}.\nDescription: {note[0]}\nDate reported: {note[1]}\nSeverity: {note[2]}\nNotes: {note[3]}\n\n"
+                count += 1
 
         illnesses = ""
         if animal_record.get("illnesses") == []:
             illnesses += "None\n\n"
         else:
+            count = 1
             for note in animal_record.get("illnesses"):
-                illnesses += f"Description: {note[0]}\nDate reported: {note[1]}\nSeverity: {note[2]}\nNotes: {note[3]}\n\n"
+                illnesses += f"{count}.\nDescription: {note[0]}\nDate reported: {note[1]}\nSeverity: {note[2]}\nNotes: {note[3]}\n\n"
+                count += 1
 
         behavioural_concerns = ""
         if animal_record.get("behavioural_concerns") == []:
             behavioural_concerns += "None\n\n"
         else:
+            count = 1
             for note in animal_record.get("behavioural_concerns"):
-                behavioural_concerns += f"Description: {note[0]}\nDate reported: {note[1]}\nSeverity: {note[2]}\nNotes: {note[3]}\n\n"
+                behavioural_concerns += f"{count}.\nDescription: {note[0]}\nDate reported: {note[1]}\nSeverity: {note[2]}\nNotes: {note[3]}\n\n"
+                count += 1
 
         # Print results.
         print(f"---Report for: {self.__name} (ID-{self.__id})---\n\n"
               f"INJURIES\n{injuries}ILLNESSES\n{illnesses}BEHAVIOURAL CONCERNS\n{behavioural_concerns}")
 
     def species_report(self):
+        '''
+        This method prints the records for all animal objects of the same species.
+        It takes no parameters and returns nothing.
+        '''
         print(f"------Species Report: {self.__species}------\n")
         for key in Animal.__notes:
             if self.__species in key:
@@ -211,6 +271,10 @@ class Animal(ABC):
                       f"INJURIES\n{injuries}ILLNESSES\n{illnesses}BEHAVIOURAL CONCERNS\n{behavioural_concerns}")
 
     def animals_report(self):
+        '''
+        This method prints the records of all instantiated animal objects, grouping by species.
+        It takes no parameters and returns nothing.
+        '''
         print(f"------Report for all Animals------\n")
         for species in Animal.__species:
             print(f"-----SPECIES: {species}-----\n")
